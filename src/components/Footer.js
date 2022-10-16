477717c4a942d263635dc02721ee61ccbff243d2
PSatt <parkers205@gmail.com>
2022-10-16 18:59:45 -0500

    product hunt badge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 function Footer() {
+    const productHuntStyle = {
+        width: "187px",
+        height: "40px"
+    };
+
     return (
         <footer className="footer">
             <div className="footer-container">
                 <h5>
                 Created & Licensed by Parker Satterfield
                 </h5>
+                <a href="https://www.producthunt.com/posts/xml-to-sql?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-xml&#0045;to&#0045;sql" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=363681&theme=light" alt="XML&#0032;to&#0032;SQL - Convert&#0032;XML&#0032;files&#0032;to&#0032;a&#0032;SQL&#0032;database | Product Hunt" style={productHuntStyle} width="250" height="54" />
+                </a>
                 <h6>
                 ©️ All Rights Reserved
                 </h6>
